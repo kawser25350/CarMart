c3ec77930ed6741f48d970801b11b8f159c87b73
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2026-06-11 00:31:30 +0600

    car order functionality added

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import FormView,View
+from .forms import OrderForm
+from car.models import Car
+from .models import Order
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class OrderView(View):
+    
+    def post(self,request,pk):
+
+        car=get_object_or_404(Car,pk=pk)
+
+        
+        if car.quantity > 0 : 
+            Order.objects.create(
+                user=request.user,
+                car=car,
+                price=car.price
+            )
+            car.quantity-=1
+            car.save()
+        return redirect("car_details",pk=car.pk)
